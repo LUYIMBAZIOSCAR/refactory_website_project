7f2dee653d26b1755946502da4880cdc39bc4278
luyimbazioscar <luyimbazioscar53@gmail.com>
2026-05-05 23:38:02 -0700

    added image slider

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('Oscar')
+// image slider
+
+// getting the element with class hero from the DOM
+const hero=document.querySelector('.hero')
+
+// defining the local paths to the images
+const images=[
+    "images/image1.jpeg",
+    "images/image2.jpeg",
+    "images/image3.jpg",
+    "images/image4.jpeg",
+]
+
+let index=0
+
+function changeBackground(){
+    // updating the url of the image
+    hero.style.backgroundImage=`url('${images[index]}')`
+
+    //  Increment index, then use Remainder (%) to loop back to 0
+    index=(index + 1) % images.length;
+}
+// change background every 4 seconds
+setInterval(changeBackground,4000)
+changeBackground()
+
